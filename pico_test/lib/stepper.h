5a8affd95bad78a71d90e9e18c1b59aca7f31438
nuoo1125 <touma201125@gmail.com>
2024-09-19 01:11:56 +0900

    Add files via upload

---
+++ pico_test/lib/stepper.h
@@ -0,0 +1,119 @@
+#include "pico/stdlib.h"
+#include "hardware/pwm.h"
+#include "pico/binary_info.h"
+#include "hardware/i2c.h"
+#include "hardware/spi.h"
+#include "hardware/adc.h"
+
+#define clock_l 20
+#define direction_l  21
+#define clock_r 18
+#define direction_r 19 
+#define reset 16 
+void stepper_setup(){
+    gpio_init(clock_l);
+    gpio_init(clock_r);
+    gpio_init(direction_l);
+    gpio_init(direction_r);
+    gpio_set_function(clock_l,GPIO_FUNC_PWM);
+    gpio_set_function(clock_r,GPIO_FUNC_PWM);
+    gpio_set_dir(direction_l,GPIO_OUT);
+    gpio_set_dir(direction_r,GPIO_OUT);
+}
+void stepper_slow(bool forward_l,bool forward_r){
+    if(forward_l == 1 && forward_r == 1){
+        gpio_put(direction_l,1);
+        gpio_put(direction_r,0);        
+    }
+    if(forward_l == 0 && forward_r == 1){
+        gpio_put(direction_l,0);
+        gpio_put(direction_r,0);        
+    }
+    if(forward_l == 0 && forward_r == 0){
+        gpio_put(direction_l,0);
+        gpio_put(direction_r,1);        
+    }
+    if(forward_l == 1 && forward_r == 0){
+        gpio_put(direction_l,1);
+        gpio_put(direction_r,1);        
+    }
+    uint slice_num_l = pwm_gpio_to_slice_num(clock_l);
+    uint chan_l = pwm_gpio_to_channel(clock_l);
+    pwm_set_clkdiv(slice_num_l,3500);
+    pwm_set_wrap(slice_num_l,1250);
+    pwm_set_chan_level(slice_num_l, chan_l, 500);
+    uint slice_num_r = pwm_gpio_to_slice_num(clock_r);
+    uint chan_r = pwm_gpio_to_channel(clock_r);
+    pwm_set_clkdiv(slice_num_r,3500);
+    pwm_set_wrap(slice_num_r, 1250);
+    pwm_set_chan_level(slice_num_r, chan_r, 500);
+    pwm_set_enabled(slice_num_l,true);
+    pwm_set_enabled(slice_num_r, true);
+}
+void stepper_fast(bool forward_l,bool forward_r){
+    if(forward_l == 1 && forward_r ==1){
+        gpio_put(direction_l,1);
+        gpio_put(direction_r,0);        
+    }
+    if(forward_l == 0 && forward_r ==1){
+        gpio_put(direction_l,0);
+        gpio_put(direction_r,0);
+    }
+    if(forward_l == 0 && forward_r == 0){
+        gpio_put(direction_l,0);
+        gpio_put(direction_r,1);        
+    }
+    if(forward_l == 1 && forward_r ==0){
+        gpio_put(direction_l,1);
+        gpio_put(direction_r,1);        
+    }
+    uint slice_num_l = pwm_gpio_to_slice_num(clock_l);
+    uint chan_l = pwm_gpio_to_channel(clock_l);
+    pwm_set_clkdiv(slice_num_l,3500);
+    pwm_set_wrap(slice_num_l,1000);
+    pwm_set_chan_level(slice_num_l, chan_l, 250);
+    uint slice_num_r = pwm_gpio_to_slice_num(clock_r);
+    uint chan_r = pwm_gpio_to_channel(clock_r);
+    pwm_set_clkdiv(slice_num_r,3500);
+    pwm_set_wrap(slice_num_r, 1000);
+    pwm_set_chan_level(slice_num_r, chan_r, 250);
+    pwm_set_enabled(slice_num_l,true);
+    pwm_set_enabled(slice_num_r, true);
+}
+void stepper_angle(int16_t angle_l,int16_t angle_r){
+    if(angle_l > 0 && angle_r >0){
+        gpio_put(direction_l,1);
+        gpio_put(direction_r,0);        
+    }
+    if(angle_l < 0 && angle_r >0){
+        gpio_put(direction_l,0);
+        gpio_put(direction_r,0);
+        angle_l *= -1;   
+    }
+    if(angle_l < 0 && angle_r < 0){
+        gpio_put(direction_l,0);
+        gpio_put(direction_r,1); 
+        angle_l *= -1;
+        angle_r *= -1;       
+    }
+    if(angle_l >0 && angle_r < 0){
+        gpio_put(direction_l,0);
+        gpio_put(direction_r,1);        
+        angle_r *= -1;
+    }
+    uint slice_num_l = pwm_gpio_to_slice_num(clock_l);
+    uint chan_l = pwm_gpio_to_channel(clock_l);
+    pwm_set_clkdiv(slice_num_l,3500);
+    pwm_set_wrap(slice_num_l,499);
+    pwm_set_chan_level(slice_num_l, chan_l, 250);
+    uint slice_num_r = pwm_gpio_to_slice_num(clock_r);
+    uint chan_r = pwm_gpio_to_channel(clock_r);
+    pwm_set_clkdiv(slice_num_r,3500);
+    pwm_set_wrap(slice_num_r, 499);
+    pwm_set_chan_level(slice_num_r, chan_r, 250);
+    pwm_set_enabled(slice_num_l, true);
+    pwm_set_enabled(slice_num_r, true);
+    sleep_ms((angle_l/1.8)*14);
+    pwm_set_enabled(slice_num_l, false);
+    pwm_set_enabled(slice_num_r, false);
+}
---
+++ pico_test/test_01.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "pico/binary_info.h"
+#include "hardware/spi.h"
+#include "lib/mcp3208.h"
+#include "lib/stepper.h"
+#define shiki 800
+int data[6];
+void photo(){
+    mcp3x08_init();
+    for(int i=2;i<8;i++){
+        data[i-2] = mcp3208_read(i);
+    }
+}
+void linetrace(){
+    photo();
+    if((data[0]>shiki)&&(data[5]<shiki))stepper_slow(true,false);
+    if((data[0]<shiki)&&(data[5]>shiki))stepper_slow(false,true);
+    else stepper_slow(true,true);
+    sleep_ms(500);
+}
+
+void main(){
+    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    gpio_put(LED_PIN,1);
+    stepper_setup();
+    stepper_slow(true,true);
+}
