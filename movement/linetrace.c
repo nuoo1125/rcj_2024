948c42dfaff9c815397d79beac76bbde70f96f5b
nuoo1125 <touma201125@gmail.com>
2024-09-19 19:39:31 +0900

    Update linetrace.c

--- movement/linetrace.c
+++ movement/linetrace.c
@@ -2,10 +2,11 @@
 //割り込みを設定するやつ参考俺のタクトスイッチ
 int main(){
   gpio_set_irq_enabled_with_callback(sw, GPIO_IRQ_EDGE_FALL, true, &sw_do);//sw_do関数を呼び出すみたいなやつ
+  car_state car = hol
   while(gin=false){
     checkline();
     if (green = true){
-      // 車の状態を管理するやつをtypedef enumで実装するからよろ
+      // 車の状態を管理するやつをtypedef enumで実装するからよろ できた
       if(state=left)move_left();
       if(state=right)move_right();
       if(state=turn)move_turn();
