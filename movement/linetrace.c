12a7435aacee729e13bc0b62fa847dc6fbcf6bad
aldebaranmelon <136123820+aldebaranmelon@users.noreply.github.com>
2024-09-19 18:00:05 +0900

    Update linetrace.c

--- movement/linetrace.c
+++ movement/linetrace.c
@@ -1,6 +1,7 @@
 //いったん日本語で書く
 //割り込みを設定するやつ参考俺のタクトスイッチ
 int main(){
+  gpio_set_irq_enabled_with_callback(sw, GPIO_IRQ_EDGE_FALL, true, &sw_do);//sw_do関数を呼び出すみたいなやつ
   while(gin=false){
     checkline();
     if (green = true){
