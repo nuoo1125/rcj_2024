a6ef589b076d35421dbe05987cb8e7217aa75d14
aldebaranmelon <136123820+aldebaranmelon@users.noreply.github.com>
2024-09-19 17:47:16 +0900

    Create linetrace.c
    適当に描いただけこれをもとに関数とかつくってほしい

---
+++ movement/linetrace.c
@@ -0,0 +1,15 @@
+//いったん日本語で書く
+//割り込みを設定するやつ参考俺のタクトスイッチ
+int main(){
+  while(gin=false){
+    checkline();
+    if (green = true){
+      // 車の状態を管理するやつをtypedef enumで実装するからよろ
+      if(state=left)move_left();
+      if(state=right)move_right();
+      if(state=turn)move_turn();
+  }
+    else{
+    linetrace();
+    }
+}
