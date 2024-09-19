f04f637a2540a8938007cbf151f3a1e018e28a93
nuoo1125 <touma201125@gmail.com>
2024-09-19 23:59:28 +0900

    Update linetrace.c

--- movement/linetrace.c
+++ movement/linetrace.c
@@ -2,7 +2,7 @@
 //割り込みを設定するやつ参考俺のタクトスイッチ
 int main(){
   gpio_set_irq_enabled_with_callback(sw, GPIO_IRQ_EDGE_FALL, true, &sw_do);//sw_do関数を呼び出すみたいなやつ
-  car_state car = hol
+  car_state car = carhold
   while(gin=false){
     checkline();
     if (green = true){
