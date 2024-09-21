9a10862738b9425e6abfd9a6cd845a0666a52959
nuoo1125 <touma201125@gmail.com>
2024-09-21 16:10:34 +0900

    Update i2c.py

--- camera/i2c.py
+++ camera/i2c.py
@@ -1,14 +1,14 @@
 from machine import I2C
 import time
-def init_i2c():
-    i2c = I2C(I2C.I2C0, freq=100000, scl=30, sda=31)
-    raspberry_pico_address = 0x28
 
+i2c = I2C(I2C.I2C0, freq=100000, scl=13, sda=14)
+
+raspberry_pico_address = 0x28  
 def send_data(data):
-    i2c.writeto(raspberry_pico_address, data)
-    print(f"Sent: {data}")
+    i2c.writeto(raspberry_pico_address,  bytes([data]))
+    print(data)
 
 def receive_data(num_bytes):
     data = i2c.readfrom(raspberry_pico_address, num_bytes)
-    print(f"Received: {data}")
+    print(data)
     return data
