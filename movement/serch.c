8c38e44a46613cffa5003a1820b92d679cf962a0
nuoo1125 <touma201125@gmail.com>
2024-09-22 00:41:19 +0900

    Update serch.c

--- movement/serch.c
+++ movement/serch.c
@@ -1,13 +1,42 @@
-while(gin>0 && black>0){
-  if(gin>0){
-    angle();
-    camera();
-    if(found_gin==true){
-        catch();
-        go_rescue();
+if(silver == true){
+  angle(-90);//serchの一番最初の角度
+  while(ball_silver>0){
+    camera_silver();
+    if(car == found_car){
+      angle_set();
+      dis = infinity;
+      while(dis<定数){
+        tof();
+        go_forward();
+        sleep_ms(100);//必要かはわからない
+      }
+      ball_get();
+      go_rescue();
+      ball_throw();
+      go_position();
     }
-    else(){
-      angle();
+    else{
+      angle(30);
+    }
+  }
+  angle(-90);//serchの一番最初の角度
+  while(ball_silver <= 0 && ball_black>0){
+    camera_black();
+    if(car == found_car){
+      angle_set();
+      dis = infinity;
+      while(dis<定数){
+        tof();
+        go_forward();
+        sleep_ms(100);//必要かはわからない
+      }
+      ball_get();
+      go_rescue();
+      ball_throw();
+      go_position();
+    }
+    else{
+      angle(30);
     }
   }
 }
