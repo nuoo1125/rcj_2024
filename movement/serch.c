e5b1bef2727a6af4c08658da0acb5f1ee6b99e31
nuoo1125 <touma201125@gmail.com>
2024-09-21 14:16:38 +0900

    Update serch.c

--- movement/serch.c
+++ movement/serch.c
@@ -1,1 +1,13 @@
-
+while(gin>0 && black>0){
+  if(gin>0){
+    angle();
+    camera();
+    if(found_gin==true){
+        catch();
+        go_rescue();
+    }
+    else(){
+      angle();
+    }
+  }
+}
