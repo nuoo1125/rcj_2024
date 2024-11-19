807f40a1134bde62cd7f9d17dba80bbd92e4dc7b
nuoo1125 <touma201125@gmail.com>
2024-11-20 07:58:44 +0900

    camera.c の更新

--- pico_test/lib/camera/camera.c
+++ pico_test/lib/camera/camera.c
@@ -1,6 +1,7 @@
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
 #define address 0x28
+int camera_data[1];
 void i2c_setup() {
     i2c_init(i2c1, 100*1000);
     gpio_set_function(15, GPIO_FUNC_I2C);
@@ -11,6 +12,6 @@ void i2c_setup() {
 void i2c_send(uint8_t data){
     i2c_write_blocking(i2c1,address,data,1,false);
 }
-void i2c_receive(uint8_t buffer){
-    i2c_read_blocking(i2c1, address, buffer, 1, false);
+void i2c_receive(){
+    i2c_read_blocking(i2c1, address, camera_data, 1, false);
 }
